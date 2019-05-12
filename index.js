a425946c553434401e3698c1e6b0cba4faeebc9c
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-05-13 00:27:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImage1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImage2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Won"
+}
+else if (randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Won";
+}
+
+else{
+  document.querySelector("h1").innerHTML = "Draw";
+}
